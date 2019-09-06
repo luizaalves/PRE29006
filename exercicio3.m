98526c1f54fb85be9aafb4064872d567b127dc4c
Luiza <l.uhzinha@hotmail.com>
2019-09-06 08:19:59 -0300

    Finalizando exercicio 3

--- exercicio3.m
+++ exercicio3.m
@@ -2,14 +2,23 @@ clear all;
 clc;
 
   #Jogar o dado: D ~ Unif({1,2,3})
-  #randi([1 3],) #primeiro argumento da funçao e o vetor, num. de linhas e num de colunas
+
   Nexp =10;
 
   D = randi([1 3], 1, Nexp);
-  X = zeros(1,Nexp); #pre alocando um vetor de zeros para deixar o programa mais rapido
-  #evita-se o uso do for em matlab/octave porque deixa mais lento
+  X = zeros(1,Nexp); %pre alocando um vetor de zeros para deixar o programa mais rapido
+  SX = [0, 1, 2, 3]
+  
+  %evita-se o uso do for em matlab/octave porque deixa mais lento
   for i = 1 : Nexp 
-    #lançar uma moeda honesta D(i) vezes
+    %lançar uma moeda honesta D(i) vezes
     moedas = randi([0 1], 1, D(i));
-    X(i) = sum(moedas)
+    X(i) = sum(moedas);
   end 
+  
+  histograma_X = hist(X, SX);
+  
+  pmf_X_simul = histograma_X / Nexp;
+  
+  bar(SX,pmf_X_simul) %plotando em formas de barra
+  
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 doc geornd > mostra como usar a função
-ex: geornd(0.1)
+ex: geornd(-1.1)
 
 Resolver o exercício 3 da lista
 
+bins > como divide o eixo xhistograma_X = hist(X,[0.5,2.5])
+
+randi([1 3],) primeiro argumento da funçao e o vetor, num. de linhas e num de colunas
 
